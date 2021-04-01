9a55743176f81bb4fd8fe0204bd69c371d914c6b
longvision <r.n.hori@gmail.com>
2021-04-01 12:16:39 -0300

    feat: privacy policy update

---
+++ src/App.js
@@ -0,0 +1,273 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Política de Privacidade</h1>
+        <div id="content">
+          <h2>1 - Como coletamos informações</h2>
+          <div class="text">
+            <p>
+              Nosso principal objetivo ao coletar informações pessoais é levar
+              até você o melhor e mais útil conteúdo e serviços.
+            </p>
+            <p>
+              Existem basicamente duas formas de coletarmos informações sobre
+              nossos usuários: por meio das interações com o site e dos dados
+              fornecidos voluntariamente. Na primeira situação obtemos e
+              arquivamos estatísticas sobre as atividades de nossos usuários,
+              como, por exemplo, quais são as páginas mais visitadas e o tempo
+              gasto em cada seção. Na segunda, obtemos informações fornecidas
+              mediante cadastramento, pesquisas, promoções e outros, sempre com
+              seu consentimento.
+            </p>
+          </div>
+          <h2>2 - Como usamos esses dados</h2>
+          <div class="text">
+            <p>
+              Utilizamos as informações genéricas que obtemos para continuamente
+              melhorar nosso site, veicular publicidade e oferecer alternativas
+              de comércio. Além disso, usamos os dados (tanto genéricos quanto
+              pessoais) para melhorar sua experiência, oferecendo conteúdo,
+              propaganda e produtos que acreditamos ser relevantes para você.
+            </p>
+            <p>
+              Utilizamos seu e-mail para enviar produtos do site (como nosso
+              boletim semanal) ou para divulgar promoções e concursos, desde que
+              solicitado por você. Nosso objetivo é retribuir sua atenção
+              encaminhando-lhe ofertas diferenciadas, sempre que possível com
+              benefí cios exclusivos.
+            </p>
+            <p>
+              Os dados que compartilhamos com terceiros são apenas genéricos e
+              agregados, provenientes das interações dos usuários com os sites.
+              Por exemplo, podemos dizer a um anunciante que um determinado
+              número de pessoas visitou determinada seção ou se cadastrou em
+              nossos sites. Porém, não divulgamos nenhuma informação que possa
+              identificar essas pessoas, ou seja, nunca divulgamos seu e-mail,
+              nome ou qualquer outro dado que possa identificá-lo.
+            </p>
+          </div>
+          <h2>3 - Cookies</h2>
+          <div class="text">
+            <p>
+              Cookies são pequenos arquivos alocados em seu computador. Ele
+              permite que sua senha seja guardada, evitando que você tenha de
+              redigitá-la diversas vezes. Além disso, podemos entender melhor
+              como os usuários interagem com os sites, ajudando-nos a oferecer
+              conteúdos, publicidade e produtos que correspondam a seus
+              interesses e necessidades.
+            </p>
+            <p>
+              Nós também usamos cookies para evitar fraudes no site e levar até
+              você informações precisas da seguinte forma:
+            </p>
+            {/* <p>
+              Quando você avalia uma receita, nós gravamos um cookie no seu
+              computador para saber que você jã registrou sua opinião sobre
+              aquela receita, ou já votou em uma certa enquete, evitando assim
+              que pessoas mal intencionadas votem diversas vezes na mesma
+              enquete ou alterem deliberadamente a avaliação geral de uma
+              receita.
+            </p>
+            <p>
+              Você pode optar por não aceitar nossos cookies (mediante
+              configuração de seu browser), mas algumas áreas de nosso site
+              podem não funcionar corretamente.
+            </p> */}
+          </div>
+          <h2>4 - Segurança</h2>
+          <div class="text">
+            <p>
+              Para garantir que somente pessoas autorizadas tenham acesso às
+              informações, garantir a coerência dos dados e assegurar o uso
+              correto das informações, utilizamos as melhores tecnologias do
+              mercado e uma equipe dedicada a gerenciar as informações que
+              coletamos online, garantindo assim a segurança dos seus dados.
+            </p>
+          </div>
+          <h2>5 - Direitos autorais e propriedade intelectual</h2>
+          <div class="text">
+            <p>
+              Todo o conteúdo exibido em nosso site está protegido por direitos
+              autorais ou outros direitos de propriedade intelectual. Tais
+              direitos pertencem à Grammage ou a terceiros, que outorgaram ao
+              Grammage as devidas autorizações e licenças para exibir tais
+              conteúdos livremente.
+            </p>
+            <p>
+              A reprodução, a qualquer título, de conteúdos do site, no todo ou
+              em parte, está expressamente proibida, salvo mediante prévia e
+              expressa autorização por escrito pelo Grammage, sob as penas da
+              legislação civil e criminal aplicável (em especial a Lei nº
+              9.610/1998).
+            </p>
+          </div>
+          <h2>6 – Conteúdos enviados pelo usuário</h2>
+          <div class="text">
+            <p>
+              Quando você submete suas receitas à Grammage, você nos concede
+              automaticamente uma licença gratuita, exclusiva, não revogável,
+              global e perpétua sobre todo o conteúdo submetido, autorizando ao
+              Grammage a livre divulgação, transmissão, cessão, licenciamento,
+              veiculação, exibição, edição, reformulação, readaptação e
+              comercialização, a qualquer título e para quaisquer fins, de tais
+              conteúdos, com quaisquer terceiros e em quaisquer mídias e
+              suportes, hoje ou no futuro existentes. Essa licença nos permite,
+              ainda, modificar a receita e qualquer conteúdo que a acompanhe
+              (inclusive texto, fotografias, etc), gravar a receita em vídeo e
+              utilizá-la para a promoção do site ou de produtos de qualquer
+              marca. Além disso, ao submeter suas receitas, você nos concede uma
+              autorização de uso do seu nome de usuário, o que nos permite (mas
+              não nos obriga a) exibi-lo e associá-lo ao conteúdo por você
+              submetido.
+            </p>
+            <p>
+              Para tanto, ao enviar receitas ou conteúdos de qualquer tipo ao
+              Grammage, o usuário expressamente declara, desde já, ser o único e
+              exclusivo responsável por tais conteúdos e possuir todos os
+              direitos a eles inerentes ou relacionados – incluindo, sem
+              limitação, direitos autorais, direitos conexos, direitos de uso de
+              imagem, de nome e dados biográficos –, podendo outorgá-los e
+              licenciá-los livremente à Grammage, sem que haja qualquer violação
+              de direitos de terceiros em tal outorga e em toda utilização
+              posteriormente conduzida pelo Grammage.
+            </p>
+          </div>
+        </div>
+      </header>
+      <header className="App-header">
+        <h1>Privacy Policy</h1>
+        <div id="content">
+          <h2>1 - How we collect information</h2>
+          <div class="text">
+            <p>
+              Nosso principal objetivo ao coletar informações pessoais é levar
+              até você o melhor e mais útil conteúdo e serviços.
+            </p>
+            <p>
+              There are basically two ways we can collect information about our
+              users: through interactions with the website and data provided
+              voluntarily. In the first situation we obtain and we archive
+              statistics about our users' activities, such as what are the most
+              visited pages and the time spent on each section. In the second,
+              we get information provided through registration, surveys,
+              promotions and others, always with your consent.
+            </p>
+          </div>
+          <h2>2 - How we use those data</h2>
+          <div class="text">
+            <p>
+              We use the generic information we obtain to continuously improve
+              our website, run advertising and offer alternatives of trade. In
+              addition, we use the data (generic and for improve your experience
+              by offering content, advertising and products that we believe are
+              relevant to you.
+            </p>
+            <p>
+              Utilizamos seu e-mail para enviar produtos do site (como nosso
+              boletim semanal) ou para divulgar promoções e concursos, desde que
+              solicitado por você. Nosso objetivo é retribuir sua atenção
+              encaminhando-lhe ofertas diferenciadas, sempre que possível com
+              benefí cios exclusivos.
+            </p>
+            <p>
+              The data we share with third parties is only generic and
+              aggregates, arising from user interactions with the sites. For
+              example, we can tell an advertiser that a particular number of
+              people visited a certain section or registered in our sites.
+              However, we do not disclose any information that could identify
+              these people, that is, we never divulge your email, name or any
+              other data that can identify you.
+            </p>
+          </div>
+          <h2>3 - Cookies</h2>
+          <div class="text">
+            <p>
+              Cookies are small files placed on your computer. He allows your
+              password to be saved, preventing you from having to retype it
+              several times. In addition, we can better understand how users
+              interact with websites, helping us to deliver content, advertising
+              and products that match your needs interests and needs.
+            </p>
+            <p>
+              We also use cookies to prevent fraud on the site and to take up to
+              you accurate information as follows:
+            </p>
+            {/* <p>
+              Quando você avalia uma receita, nós gravamos um cookie no seu
+              computador para saber que você jã registrou sua opinião sobre
+              aquela receita, ou já votou em uma certa enquete, evitando assim
+              que pessoas mal intencionadas votem diversas vezes na mesma
+              enquete ou alterem deliberadamente a avaliação geral de uma
+              receita.
+            </p>
+            <p>
+              Você pode optar por não aceitar nossos cookies (mediante
+              configuração de seu browser), mas algumas áreas de nosso site
+              podem não funcionar corretamente.
+            </p> */}
+          </div>
+          <h2>4 - Security</h2>
+          <div class="text">
+            <p>
+              To ensure that only authorized persons have access to the
+              information, ensure data coherence and ensure the use of
+              information, we use the best technologies in the market and a team
+              dedicated to managing the information that we collect online, thus
+              ensuring the security of your data.
+            </p>
+          </div>
+          <h2>5 - Intelectual Property</h2>
+          <div class="text">
+            <p>
+              All content displayed on our website is protected by rights
+              copyright or other intellectual property rights. Such rights
+              belong to Grammage or to third parties, who have granted the
+              Grammage the appropriate authorizations and licenses to display
+              such contents freely.
+            </p>
+            <p>
+              The reproduction, in any capacity, of the contents of the website,
+              in whole or in part, it is expressly prohibited, except with prior
+              and express written authorization by Grammage, under penalty of
+              applicable civil and criminal law
+            </p>
+          </div>
+          <h2>6 – Content provided by users</h2>
+          <div class="text">
+            <p>
+              When you submit your recipes to Grammage, you automatically grant
+              us a free, exclusive, non-revocable, global and perpetual license
+              on all submitted content, allowing Grammage to freely disclose,
+              transmit, assign, license, convey, display, edit, reformulate ,
+              retrofitting and marketing, for any purpose and for any purpose,
+              of such content, with any third parties and in any existing media
+              and supports, today or in the future. This license also allows us
+              to modify the recipe and any accompanying content (including text,
+              photographs, etc.), record the recipe on video and use it to
+              promote the website or products of any brand. In addition, by
+              submitting your recipes, you grant us authorization to use your
+              username, which allows us (but does not obligate us to a) display
+              it and associate it with the content you submit.
+            </p>
+            <p>
+              Therefore, when sending recipes or content of any kind to the
+              Grammage, the user expressly declares, from now on, to be the only
+              and sole responsibility for such content and possess all the
+              inherent or related rights - including without limitation,
+              copyright, related rights, rights to use image, name and
+              biographical data - and may grant them and freely license them to
+              Grammage, without any violation rights of third parties in such
+              grant and in all use subsequently conducted by Grammage.
+            </p>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/PrivaryPolicy.js
@@ -0,0 +1,136 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function Privary() {
+  return (
+    <div id="content">
+      <h1>1 - Como coletamos informações</h1>
+      <div class="text">
+        <p>
+          Nosso principal objetivo ao coletar informações pessoais é levar até
+          você o melhor e mais útil conteúdo e serviços.
+        </p>
+        <p>
+          Existem basicamente duas formas de coletarmos informações sobre nossos
+          usuários: por meio das interações com o site e dos dados fornecidos
+          voluntariamente. Na primeira situação obtemos e arquivamos
+          estatísticas sobre as atividades de nossos usuários, como, por
+          exemplo, quais são as páginas mais visitadas e o tempo gasto em cada
+          seção. Na segunda, obtemos informações fornecidas mediante
+          cadastramento, pesquisas, promoções e outros, sempre com seu
+          consentimento.
+        </p>
+      </div>
+      <h1>2 - Como usamos esses dados</h1>
+      <div class="text">
+        <p>
+          Utilizamos as informações genéricas que obtemos para continuamente
+          melhorar nosso site, veicular publicidade e oferecer alternativas de
+          comércio. Além disso, usamos os dados (tanto genéricos quanto
+          pessoais) para melhorar sua experiência, oferecendo conteúdo,
+          propaganda e produtos que acreditamos ser relevantes para você.
+        </p>
+        <p>
+          Utilizamos seu e-mail para enviar produtos do site (como nosso boletim
+          semanal) ou para divulgar promoções e concursos, desde que solicitado
+          por você. Nosso objetivo é retribuir sua atenção encaminhando-lhe
+          ofertas diferenciadas, sempre que possível com benefí cios exclusivos.
+        </p>
+        <p>
+          Os dados que compartilhamos com terceiros são apenas genéricos e
+          agregados, provenientes das interações dos usuários com os sites. Por
+          exemplo, podemos dizer a um anunciante que um determinado número de
+          pessoas visitou determinada seção ou se cadastrou em nossos sites.
+          Porém, não divulgamos nenhuma informação que possa identificar essas
+          pessoas, ou seja, nunca divulgamos seu e-mail, nome ou qualquer outro
+          dado que possa identificá-lo.
+        </p>
+      </div>
+      <h1>3 - Cookies</h1>
+      <div class="text">
+        <p>
+          Cookies são pequenos arquivos alocados em seu computador. Ele permite
+          que sua senha seja guardada, evitando que você tenha de redigitá-la
+          diversas vezes. Além disso, podemos entender melhor como os usuários
+          interagem com os sites, ajudando-nos a oferecer conteúdos, publicidade
+          e produtos que correspondam a seus interesses e necessidades.
+        </p>
+        <p>
+          Nós também usamos cookies para evitar fraudes no site e levar até você
+          informações precisas da seguinte forma:
+        </p>
+        {/* <p>
+              Quando você avalia uma receita, nós gravamos um cookie no seu
+              computador para saber que você jã registrou sua opinião sobre
+              aquela receita, ou já votou em uma certa enquete, evitando assim
+              que pessoas mal intencionadas votem diversas vezes na mesma
+              enquete ou alterem deliberadamente a avaliação geral de uma
+              receita.
+            </p>
+            <p>
+              Você pode optar por não aceitar nossos cookies (mediante
+              configuração de seu browser), mas algumas áreas de nosso site
+              podem não funcionar corretamente.
+            </p> */}
+      </div>
+      <h1>4 - Segurança</h1>
+      <div class="text">
+        <p>
+          Para garantir que somente pessoas autorizadas tenham acesso às
+          informações, garantir a coerência dos dados e assegurar o uso correto
+          das informações, utilizamos as melhores tecnologias do mercado e uma
+          equipe dedicada a gerenciar as informações que coletamos online,
+          garantindo assim a segurança dos seus dados.
+        </p>
+      </div>
+      <h1>5 - Direitos autorais e propriedade intelectual</h1>
+      <div class="text">
+        <p>
+          Todo o conteúdo exibido em nosso site está protegido por direitos
+          autorais ou outros direitos de propriedade intelectual. Tais direitos
+          pertencem à Grammage ou a terceiros, que outorgaram ao Grammage as
+          devidas autorizações e licenças para exibir tais conteúdos livremente.
+        </p>
+        <p>
+          A reprodução, a qualquer título, de conteúdos do site, no todo ou em
+          parte, está expressamente proibida, salvo mediante prévia e expressa
+          autorização por escrito pelo Grammage, sob as penas da legislação
+          civil e criminal aplicável (em especial a Lei nº 9.610/1998).
+        </p>
+      </div>
+      <h1>6 – Conteúdos enviados pelo usuário</h1>
+      <div class="text">
+        <p>
+          Quando você submete suas receitas à Grammage, você nos concede
+          automaticamente uma licença gratuita, exclusiva, não revogável, global
+          e perpétua sobre todo o conteúdo submetido, autorizando ao Grammage a
+          livre divulgação, transmissão, cessão, licenciamento, veiculação,
+          exibição, edição, reformulação, readaptação e comercialização, a
+          qualquer título e para quaisquer fins, de tais conteúdos, com
+          quaisquer terceiros e em quaisquer mídias e suportes, hoje ou no
+          futuro existentes. Essa licença nos permite, ainda, modificar a
+          receita e qualquer conteúdo que a acompanhe (inclusive texto,
+          fotografias, etc), gravar a receita em vídeo e utilizá-la para a
+          promoção do site ou de produtos de qualquer marca. Além disso, ao
+          submeter suas receitas, você nos concede uma autorização de uso do seu
+          nome de usuário, o que nos permite (mas não nos obriga a) exibi-lo e
+          associá-lo ao conteúdo por você submetido.
+        </p>
+        <p>
+          Para tanto, ao enviar receitas ou conteúdos de qualquer tipo ao
+          Grammage, o usuário expressamente declara, desde já, ser o único e
+          exclusivo responsável por tais conteúdos e possuir todos os direitos a
+          eles inerentes ou relacionados – incluindo, sem limitação, direitos
+          autorais, direitos conexos, direitos de uso de imagem, de nome e dados
+          biográficos –, podendo outorgá-los e licenciá-los livremente à
+          Grammage, sem que haja qualquer violação de direitos de terceiros em
+          tal outorga e em toda utilização posteriormente conduzida pelo
+          Grammage.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Privary;
